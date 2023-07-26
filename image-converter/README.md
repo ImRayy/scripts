87621835a96e01fa335e591d4eb700fe06cfb1b1
ImRayy <souradeep20roy01@gmail.com>
2023-07-26 13:21:22 +0530

    Updated image-converter

--- image-converter/README.md
+++ image-converter/README.md
@@ -1,5 +1,7 @@
-## Requirements
+## What is this?
+A simple script to convert files to WEBP and decompress WEBP in bulk
 
-1. cwebp (for converting to webp)
-2. dwebp (decompress webp)
+## Requirements
+1. cwebp (for converting to WEBP)
+2. dwebp (decompress WEBP)
 3. [gum](https://github.com/charmbracelet/gum)
--- image-converter/main.sh
+++ image-converter/main.sh
@@ -1,35 +1,66 @@
 #!/bin/bash
 
-function main() {
+function welcome_msg() {
+    gum style --margin "1 0" --padding "1 2" --border double --border-foreground 212 "$(gum style --foreground 212 'Image Converter')"
+}
+
+function loading() {
+    gum spin --spinner dot --title "Converting..." --
+}
+
+function convert() {
     FUNC=$(gum choose {"1. Convert to WEBP","2. Convert from WEBP"} | cut -d '.' -f 1)
 
     case "$FUNC" in
     1)
         echo "Lossless not recommended for non PNG files"
         local quality=$(gum choose {0,75,100,lossless})
+        function hello() {
+            tree /home/$USER/
+        }
+
+        local total_files=$(find $1/ -type f | wc -l)
+        local current_file=1
         for file in $1/*; do
+
             local filename=$(awk -F '/' '{print $NF}' <<<$file | cut -d '.' -f 1)
+            echo "$(tput cuu1 & tput el & gum style --foreground 212 ${current_file}) of $(gum style --foreground 212 ${total_files})"
 
             if [[ $quality != "" ]] && [[ $quality != "lossless" ]]; then
-                cwebp $file -o $1/"${filename}.webp" -q $quality
-                echo "non lossless"
+                gum spin --spinner dot --title "Converting..." -- \
+                    cwebp $file -o $1/"${filename}.webp" -q $quality
             elif [[ $quality == "lossless" ]]; then
-                cwebp $file -o $1/"${filename}.webp" -lossless
+                gum spin --spinner dot --title "Converting..." -- \
+                    cwebp $file -o $1/${filename}.webp -lossless
             else
                 exit
             fi
+            ((current_file = current_file + 1))
         done
+        echo ""
+        gum style --foreground 40 "Successful!"
+        echo ""
         ;;
     2)
         for file in $1/*; do
             local filename=$(awk -F '/' '{print $NF}' <<<$file | cut -d '.' -f 1)
-            dwebp $file -o $1/"${filename}.png"
+            gum spin --spinner dot --title "Converting..." -- \
+                dwebp $file -o $1/"${filename}.png"
         done
         ;;
     *)
         echo "None selected"
         ;;
     esac
+}
+
+function main() {
+    if [[ -z "$1" ]]; then
+        echo -e "> bash main.sh $(gum style --foreground 212 "_files_path_")"
+    else
+        welcome_msg
+        convert $1
+    fi
 }
 
 main $1
